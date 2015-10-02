98d864ca1808992b0d04dd013cd0d5cb3d1e4269
Hana Nagel <nagel.hana@gmail.com>
2015-10-02 15:13:01 -0400

    Here's what I have done for 1 and 2 - I tested both and they seem to work ok, I think I probably need to add more comments though.

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-""" Assignment 1, Exercise 1, INF1340, Fall, 2015. Stock transactions 
+""" Assignment 1, Exercise 1, INF1340, Fall, 2015. Stock transactions
 
 This module prints the amount of money that Lakshmi has remaining
 after the stock transactions
@@ -13,5 +13,32 @@ __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 
-money = 1000.00
-print(money)
+#money = 1000.00
+#print(money)
+
+#Lakshmi's stock purchase
+shares_bought = 2000.00
+buy_price = 900.00
+stock_buy_value = shares_bought * buy_price
+stockbroker_buy_commission = 0.03 * (buy_price * shares_bought)
+stock_buy_revenue = (buy_price) * (shares_bought) + (stockbroker_buy_commission)
+print "Lakshmi bought", shares_bought, "stocks for", buy_price,"each, for a total of", stock_buy_value,"."
+print "Lakshmi paid her stockbroker",stockbroker_buy_commission,"for the purchase."
+print "After paying her stockbroker, Lakshmi's total payment was", stock_buy_revenue,"."
+
+print"\n"
+
+#Lakshmi's stock sale
+shares_sold = 2000.00
+sale_price = 942.75
+stock_sale_value = shares_sold * sale_price
+stockbroker_sale_commission = 0.03 * (sale_price * shares_sold)
+stock_sale_revenue = (sale_price) * (shares_sold) - (stockbroker_sale_commission)
+print "Lakshmi sold" ,shares_sold, "shares for" ,sale_price, "each, for a total of" , stock_sale_value, "."
+print "Lakshmi paid her stockbroker" ,stockbroker_sale_commission, "for the sale."
+print "After paying her stockbroker, Lakshmi's total revenue from the sale was", stock_sale_revenue,"."
+
+print"\n"
+
+#final calculation of Lakshmi's
+print "After Lakshmi sold the stocks and paid the broker commission, she had", stock_buy_revenue - stock_sale_revenue, "left."
--- exercise2.py
+++ exercise2.py
@@ -24,8 +24,30 @@ def name_that_shape():
     Errors:
 
     """
-
-    print("Error")
-
+    answer = raw_input("Please enter how many sides your shape has, and then hit 'Enter'")
+    #The following answer is a cool party conversation starter. Try it sometime!
+    if answer == '2':
+        print "Fun fact - a polygon is a plane (2D) shape with straight sides. To be a regular polygon all the sides and angles must be the same."
+    elif answer == '3':
+        print "Triangle"
+    elif answer == '4':
+        print "Rectangle or square"
+    elif answer == '5':
+        print "Pentagon"
+    elif answer == '6':
+        print "Hexagon"
+    elif answer == '7':
+        print "Heptagon"
+    elif answer == '8':
+        print "Heptagon"
+    elif answer == '9':
+        print "Nonagon"
+    elif answer == '10':
+        print "Decagon"
+    #More fun because sometimes writing code can be dry.
+    elif answer == '42':
+        print "I always thought something was fundamentally wrong with the universe."
+    else:
+        print "Error"
 
 name_that_shape()
