3639ca7c0d761620b9595b13aeb3c5a405accbd2
SLiana <liana.sukiasyan@mail.utoronto.ca>
2015-10-06 12:40:25 -0400

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -51,3 +51,39 @@ def name_that_shape():
         print "Error"
 
 name_that_shape()
+
+
+
+
+
+ Assignment 1, Exercise 2 (October 06, 2015)
+ 
+ 
+shape_name = raw_input("Please, enter how many sides your shape has and then hit 'Enter': ")
+print "\n"
+
+# Defining the shape names.
+if shape_name == '2':
+    print "Fun fact - a polygon is a plane (2D) shape with straight sides. To be a regular polygon all the sides and angles must be the same."
+elif shape_name == '3':
+    print "Thank you for using the Shape Name Finder. The shape you have chosen is 'Triangle'."
+elif shape_name == '4':
+    print "Thank you for using the Shape Name Finder. The shape you have chosen is 'Rectangle or square'."
+elif shape_name == '5':
+    print "Thank you for using the Shape Name Finder. The shape you have chosen is 'Pentagon'."
+elif shape_name == '6':
+    print "Thank you for using the Shape Name Finder. The shape you have chosen is 'Hexagon'."
+elif shape_name == '7':
+    print "Thank you for using the Shape Name Finder. The shape you have chosen is 'Heptagon'."
+elif shape_name == '8':
+    print "Thank you for using the Shape Name Finder. The shape you have chosen is 'Heptagon'."
+elif shape_name == '9':
+    print "Thank you for using the Shape Name Finder. The shape you have chosen is 'Nonagon'."
+elif shape_name == '10':
+    print "Thank you for using the Shape Name Finder. The shape you have chosen is 'Decagon'."
+
+#More fun because sometimes writing code can be dry.
+elif shape_name == '42':
+    print "I always thought something was fundamentally wrong with the universe."
+else:
+    print "Error"
